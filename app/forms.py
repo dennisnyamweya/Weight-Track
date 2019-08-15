35a9b17eb1abead2b4168655a29a753251acbd11
George Charles <charlesschiedel@gmail.com>
2019-08-15 14:39:38 +0300

    I have added an email validation

--- app/forms.py
+++ app/forms.py
@@ -110,7 +110,7 @@ class RegistrationForm(FlaskForm):
         if user is not None:
             raise ValidationError('Please use a different username.')
 
-#     def validate_email(self, email):
-#         user = User.query.filter_by(email=email.data).first()
-#         if user is not None:
-#             raise ValidationError('Please use a different email address.')
+    def validate_email(self, email):
+        user = User.query.filter_by(email=email.data).first()
+        if user is not None:
+            raise ValidationError('Please use a different email address.')

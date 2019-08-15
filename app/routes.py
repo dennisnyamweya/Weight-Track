6e92f72087d6be40cc1abd8514220520046d8d79
George Charles <charlesschiedel@gmail.com>
2019-08-15 14:50:54 +0300

    I have done routing for the login page

--- app/routes.py
+++ app/routes.py
@@ -312,23 +312,23 @@ def copyto(date, meal):
         return redirect(url_for('diary', date_pick = copy_to_date))
 
 
-# @app.route('/login', methods=['GET', 'POST'])
-# def login():
-#     if current_user.is_authenticated:
-#         return redirect(url_for('home'))
-#     else:
-#         form = LoginForm()
-#         if form.validate_on_submit():
-#             user = User.query.filter_by(username=form.username.data).first()
-#             if user is None or not user.check_password(form.password.data):
-#                 flash('Invalid username or password.')
-#                 return redirect(url_for('login'))
-#             login_user(user, remember=form.remember_me.data)
-#             next_page = request.args.get('next')
-#             if not next_page or url_parse(next_page).netloc != '':
-#                 next_page = url_for('diary')
-#             return redirect(next_page)
-#         return render_template('login.html', form=form)
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if current_user.is_authenticated:
+        return redirect(url_for('home'))
+    else:
+        form = LoginForm()
+        if form.validate_on_submit():
+            user = User.query.filter_by(username=form.username.data).first()
+            if user is None or not user.check_password(form.password.data):
+                flash('Invalid username or password.')
+                return redirect(url_for('login'))
+            login_user(user, remember=form.remember_me.data)
+            next_page = request.args.get('next')
+            if not next_page or url_parse(next_page).netloc != '':
+                next_page = url_for('diary')
+            return redirect(next_page)
+        return render_template('login.html', form=form)
 
 
 @app.route('/profile')

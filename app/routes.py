10b33d210b5e21dd806a92ed0c823b2b9353d42c
George Charles <charlesschiedel@gmail.com>
2019-08-15 14:51:49 +0300

    I have done routing for the register page

--- app/routes.py
+++ app/routes.py
@@ -421,20 +421,20 @@ def macros_percent():
             return redirect(url_for('macros_percent'))
 
 
-# @app.route('/register', methods=['GET', 'POST'])
-# def register():
-#     if current_user.is_authenticated:
-#         return redirect(url_for('home'))
-#     else:
-#         form = RegistrationForm()
-#         if form.validate_on_submit():
-#             user = User(username=form.username.data, email=form.email.data)
-#             user.set_password(form.password.data)
-#             db.session.add(user)
-#             db.session.commit()
-#             flash('Congratulations, you are now a registered user!')
-#             return redirect(url_for('login'))
-#         return render_template('register.html', form=form)
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    if current_user.is_authenticated:
+        return redirect(url_for('home'))
+    else:
+        form = RegistrationForm()
+        if form.validate_on_submit():
+            user = User(username=form.username.data, email=form.email.data)
+            user.set_password(form.password.data)
+            db.session.add(user)
+            db.session.commit()
+            flash('Congratulations, you are now a registered user!')
+            return redirect(url_for('login'))
+        return render_template('register.html', form=form)
 
 
 @app.route('/logout')
